1ea4a8797db4c3d41ccbb04310d8c79aa20de14f
FedeFreire <bandoleir@gmail.com>
2023-11-15 12:56:11 -0500

    Tried YUP but didn't word. Went manual.

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,29 +1,59 @@
 const db = require("../../data/db-config");
 
-const yup = require("yup");
+// const yup = require("yup");
 
-const accountSchema = yup.object({
-  name: yup
-    .string()
-    .trim()
-    .min(3)
-    .max(100)
-    .required("name and budget are required"),
-  budget: yup
-    .number()
-    .typeError("budget of account must be a number")
-    .positive("budget of account is too large or too small")
-    .max(1000000, "budget of account is too large or too small")
-    .required("name and budget are required"),
-});
+// const accountSchema = yup.object({
+//   name: yup
+//     .string()
+//     .trim()
+//     .min(3, 'name of account must be between 3 and 100')
+//     .max(100, 'name of account must be between 3 and 100')
+//     .required("name and budget are required 1"),
+//   budget: yup
+//     .number()
+//     .typeError("budget of account must be a number")
+//     .positive("budget of account is too large or too small")
+//     .max(1000000, "budget of account is too large or too small")
+//     .required("name and budget are required"),
+// });
 
-exports.checkAccountPayload = async (req, res, next) => {
-  try {
-    await accountSchema.validate(req.body);
-    next();
-  } catch (error) {
-    return res.status(400).json({ message: error.message });
+// exports.checkAccountPayload = async (req, res, next) => {
+//   try {
+//     await accountSchema.validate(req.body);
+//     next();
+//   } catch (error) {
+//     return res.status(400).json({ message: error.message });
+//   }
+// };
+
+exports.checkAccountPayload = (req, res, next) => {
+  const { name, budget } = req.body;
+
+  if (name === undefined || budget === undefined) {
+    return res.status(400).json({ message: "name and budget are required" });
   }
+
+  const trimmedName = name.trim();
+  if (trimmedName.length < 3 || trimmedName.length > 100) {
+    return res
+      .status(400)
+      .json({ message: "name of account must be between 3 and 100" });
+  }
+
+  const budgetNumber = parseFloat(budget);
+  if (isNaN(budgetNumber)) {
+    return res
+      .status(400)
+      .json({ message: "budget of account must be a number" });
+  }
+
+  if (budgetNumber < 0 || budgetNumber > 1000000) {
+    return res
+      .status(400)
+      .json({ message: "budget of account is too large or too small" });
+  }
+
+  next();
 };
 
 exports.checkAccountId = async (req, res, next) => {
